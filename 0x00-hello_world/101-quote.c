3c4564981a3dce491d6c8f7436605e442f495dc0
Glitcodes <basseydegreat@gmail.com>
2023-02-16 14:01:58 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,23 @@
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Description: This program prints a string to the standard error
+ *     * without using printf or puts functions.
+ *      *
+ *       * Return: Always 1 (indicating an error occurred)
+ */
+
 #include <unistd.h>
 
-int main() {
-	  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	    write(STDERR_FILENO, message, sizeof(message) - 1);
-	      return 1;
+int main(void)
+{
+	  char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	    int len = 0;
+
+	      while (str[len] != '\0')
+		          len++;
+
+	        write(2, str, len);
+
+		  return (1);
 }
