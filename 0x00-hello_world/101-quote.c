dc57bb4e4a864e600b8e713f25d0bf86843b34b6
Glitcodes <basseydegreat@gmail.com>
2023-02-16 14:19:11 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,23 @@
+#include <unistd.h>
+
 /**
  *  * main - Entry point of the program
  *   *
- *    * Description: This program prints a string to the standard error
- *     * without using printf or puts functions.
- *      *
- *       * Return: Always 1 (indicating an error occurred)
+ *    * Description: Prints "and that piece of art is useful" -
+ *     * Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *      * Return: 1
  */
-
-#include <unistd.h>
-
 int main(void)
 {
-	  char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	    int len = 0;
+	    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	        int len = 0;
 
-	      while (str[len] != '\0')
-		          len++;
+		    while (str[len] != '\0')
+			        {
+					        len++;
+						    }
 
-	        write(2, str, len);
+		        write(2, str, len);
 
-		  return (1);
+			    return (1);
 }
