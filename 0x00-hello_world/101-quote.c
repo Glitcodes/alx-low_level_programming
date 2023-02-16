8138f6e1c7017269548d6ff18a84bfc9ae30f58d
Glitcodes <basseydegreat@gmail.com>
2023-02-16 14:55:42 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- *  *main - A C program that prints and that piece of art is useful"
+ *  main - A C program that prints and that piece of art is useful"
  *   * - Dora Korpar, 2015-10-19
  *    * Return: 1 (True)
- *     */
+ */
 
 int main(void)
 {
