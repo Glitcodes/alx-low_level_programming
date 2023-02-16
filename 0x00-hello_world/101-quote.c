5f89c39f6ade7eea42e2b6cebf53b0d7883baa30
Glitcodes <basseydegreat@gmail.com>
2023-02-16 15:08:45 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 
 /**
  *  * main - A C program that prints "and that piece of art is useful"
- *   *        - Dora Korpar, 2015-10-19
- *    * Return: Always 1 (Success)
+ *   *	- Dora Korpar, 2015-10-19
+ *    *
+ *     * Return: Always 1 (Success)
  */
 int main(void)
 {
-	    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			              stdout);
-	        return (1);
+		fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+						stdout);
+			return (1);
 }
