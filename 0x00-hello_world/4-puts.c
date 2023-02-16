5fb27558c94963c4c8ff0df8320aadf3bcff313c
Glitcodes <basseydegreat@gmail.com>
2023-02-16 13:36:52 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *  * main - A program that prints a line with puts function
- *   * Return: 0 (Success)
- *    */
+ *   * Return: 0 (Success)*
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
