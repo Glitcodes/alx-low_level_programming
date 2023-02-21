15bc7f9e755814fff9cb84f1df4b8e8289a65309
Glitcodes <basseydegreat@gmail.com>
2023-02-21 15:39:05 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *   */
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 void print_alphabet_x10(void)
 {
 	int count = 0;
