8fd4e7d16734120227af7444a6cc95e1ac04797a
Glitcodes <basseydegreat@gmail.com>
2023-02-21 15:49:54 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-									for (letter = 'a'; letter <= 'z'; letter++)
-										_putchar(letter);
-									_putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar('\n');
 	}
 }
