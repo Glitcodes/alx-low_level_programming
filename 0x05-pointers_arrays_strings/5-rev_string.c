da318cc32e95759258f731708213736287702768
Glitcodes <basseydegreat@gmail.com>
2023-02-27 05:13:20 -0800

    Testing code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,14 @@
  *
  * Return: 0 is success
  */
+
 void puts2(char *str)
 {
-	int string;
+		int string;
 
-	for (string = 0; str[string] != '\0'; string++)
-		if (string % 2 == 0)
-			_putchar(str[string]);
-	_putchar('\n');
+			for (string = 0; str[string] != '\0'; string++)
+					if (string % 2 == 0)
+								_putchar(str[string]);
+				_putchar('\n');
 }
+
