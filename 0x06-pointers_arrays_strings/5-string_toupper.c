2c6fde9ab359f4bae7687fdff67c852dd28193af
Glitcodes <basseydegreat@gmail.com>
2023-03-01 04:05:24 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- *  * string_toupper - change lowercase to uppercase
- *   * @s:string
- *    * Return:char
- *     */
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
 char *string_toupper(char *s)
 {
 
-		int i;
+	int i;
 
-		i = 0;
-			while (*(s + i))
-					{
-								if (*(s + i) >= 'a' && *(s + i) <= 'z')
-												*(s + i) -= 'a' - 'A';
-										i++;
-											}
-				return (s);
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
 }
