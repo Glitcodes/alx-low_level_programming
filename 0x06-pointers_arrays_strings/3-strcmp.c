8541176bb4f52e4cb7a4dbe4c4e8b1a629a984de
Glitcodes <basseydegreat@gmail.com>
2023-03-01 03:56:09 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
- *  * _strcmp - compare 2 string
- *   * @s1:string
- *    * @s2:strmp
- *     * Return:int
- *      * Bwave/ Bright
- *       */
+ * _strcmp - compare 2 string
+ * @s1:string
+ * @s2:strmp
+ * Return:int
+ ** Glitcodes
+ */
 int _strcmp(char *s1, char *s2)
 {
-		while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-				{
-							s1++;
-									s2++;
-										}
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-			if (*s1 == *s2)
-					{
-								return (0);
-									}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
 
-				else
-						{
-									return (*s1 - *s2);
-										}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
